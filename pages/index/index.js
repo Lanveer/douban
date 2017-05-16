681672f3d8304e07ce8daa05e243f495b21b087b
Lanveer <Lanveer@users.noreply.github.com>
2017-05-16 18:02:00 +0800

    豆瓣电影

---
+++ pages/index/index.js
@@ -0,0 +1,29 @@
+var API_URL = 'https://api.douban.com/v2/movie/top250';
+
+Page({
+  data: {
+  	title:"加载中。。",
+  	movies:[]
+  },
+  onLoad:function (){
+  	var that = this;
+  	wx.showToast({
+  		title:"加载中...",
+  		icon:"loading",
+  		duration:10000
+  	});
+
+  	wx.request({
+  		data:{},
+  		success:function (res){
+  			wx.hideToast();
+  			var data = res.data;
+  			console.log(data);
+  			that.setData({
+  				title:data.title,
+  				movies:data.subjects
+  			});
+  		}
+  	});
+  }
+})
